054b7fbb8b350aa8b1f6a2ebfcd1e2c8e0c669e4
Md Usman Ansari <usman.a@mtechzilla.com>
2024-03-13 11:22:56 +0530

    feat: add github link

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,14 +1,22 @@
 import Link from "next/link";
 import { Logo } from "@/components/logo";
 import { Badge } from "@/components/badge";
+import { GithubIcon } from "@/components/github-icon";
 
 export function Navigation() {
   return (
     <nav className="sticky top-0 bg-white border-b">
-      <div className="container flex items-center py-4">
+      <div className="container flex items-center py-4 gap-2">
         <Logo />
-        <Badge className="ml-2 mr-auto">Coming Soon</Badge>
-        <Link href="/blog">Blog</Link>
+        <Badge className="mr-auto">Coming Soon</Badge>
+        <Link href="/blog" className="mr-2">
+          Blog
+        </Link>
+        <Link href="https://github.com/useSaasKit" legacyBehavior>
+          <a target="_blank">
+            <GithubIcon />
+          </a>
+        </Link>
       </div>
     </nav>
   );
